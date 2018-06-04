6bb16e7c96e4f94aa6699a3269e9591c1dd7ba28
vishnu3101974 <39912913+vishnu3101974@users.noreply.github.com>
2018-06-04 07:13:07 -0700

    Add files via upload

---
+++ optic_tx_final_working.ino
@@ -0,0 +1,28 @@
+
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("Optical Tx");
+
+ 
+  mySerial.begin(4800);
+
+}
+
+void loop() 
+{ 
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
+
